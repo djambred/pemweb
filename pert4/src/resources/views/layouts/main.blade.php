bf2f9519bf0a053774e1462edcaf23078c102a01
djambred <djambred@gmail.com>
2023-04-08 20:04:57 +0700

    fresh from oven

---
+++ pert4/src/resources/views/layouts/main.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- <title>{{config('app.name')}}</title> -->
+    <title>@yield('title', $title)</title>
+    
+    @include('partials.styles')
+</head>
+<body>
+    @include('partials.header')
+    <h1>@yield('title', $title)</h1>
+    <main class="container mt-5">
+        @yield('content')
+    </main>
+</body>
+    
+    @include('partials.footer')
+    @include('partials.scripts')
+</html>
---
+++ pert4/src/resources/views/partials/footer.blade.php
@@ -0,0 +1,32 @@
+<footer>
+    <!-- Footer -->
+<footer class="text-center text-lg-start bg-light text-muted fixed-bottom">
+  <!-- Section: Social media -->
+  <section class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
+    <!-- Left -->
+    <div class="me-5 d-none d-lg-block">
+      <span>Get connected with us on social networks:</span>
+    </div>
+    <!-- Left -->
+
+    <!-- Right -->
+    <div>
+      <a href="#" class="d-inline-flex p-2" style="text-decoration: none;">
+        <i class="fab fa-google"></i>
+      </a>
+      <a href="#" class="d-inline-flex p-2" style="text-decoration: none;">
+        <i class="fab fa-github"></i>
+      </a>
+    </div>
+    <!-- Right -->
+  </section>
+  <!-- Section: Social media -->
+
+  <!-- Copyright -->
+  <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
+    © 2023 Copyright:
+    <a class="text-reset fw-bold" href="https://djambred.my.id/">Universitas Esa Unggul</a>
+  </div>
+  <!-- Copyright -->
+</footer>
+<!-- Footer -->
